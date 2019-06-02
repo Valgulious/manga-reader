2165c570e2104fd18c3c36d99beaab702c17900b
Valgulious <dmitryfedorenkof@gmail.com>
2019-06-02 13:55:42 +1000

    ;

--- src/js/components/main/Main.jsx
+++ src/js/components/main/Main.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Main extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             mangas: []
